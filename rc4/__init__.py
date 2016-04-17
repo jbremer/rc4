cde8a59704b2d4cf276c00cb7d11f085acf589af
Jurriaan Bremer <jbr@cuckoo.sh>
2016-04-17 15:14:25 +0200

    initial rc4 package

---
+++ rc4/__init__.py
@@ -0,0 +1,24 @@
+# Copyright (C) 2016 Jurriaan Bremer.
+# See the file 'LICENSE.txt' for copying permission.
+
+def rc4(data, key):
+    """RC4 encryption and decryption method."""
+    S, j, out = range(256), 0, []
+
+    for i in range(256):
+        j = (j + S[i] + ord(key[i % len(key)])) % 256
+        S[i], S[j] = S[j], S[i]
+
+    i = j = 0
+    for ch in data:
+        i = (i + 1) % 256
+        j = (j + S[i]) % 256
+        S[i], S[j] = S[j], S[i]
+        out.append(chr(ord(ch) ^ S[(S[i] + S[j]) % 256]))
+
+    return "".join(out)
+
+if __name__ == "__main__":
+    buf = rc4("Hello World", "rc4")
+    assert rc4(buf, "rc4") == "Hello World"
+    print "Ran 1 test.."
---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# Copyright (C) 2016 Jurriaan Bremer.
+# See the file 'LICENSE.txt' for copying permission.
+
+from setuptools import setup
+
+setup(
+    name="rc4",
+    version="0.1",
+    author='Jurriaan Bremer',
+    author_email="jbr@cuckoo.sh",
+    packages=[
+        "rc4",
+    ],
+    url="https://github.com/jbremer/rc4",
+    license="BSD 3-Clause",
+    description="Package which exposes the RC4 algorithm",
+)
