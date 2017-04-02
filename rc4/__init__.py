2c2977e1f3a9f2435081b0e9143bf84b98388a3e
Tithen-Firion <Tithen-Firion@users.noreply.github.com>
2017-04-02 16:06:00 +0200

    fix for Python 3

--- rc4/__init__.py
+++ rc4/__init__.py
@@ -3,7 +3,7 @@
 
 def rc4(data, key):
     """RC4 encryption and decryption method."""
-    S, j, out = range(256), 0, []
+    S, j, out = list(range(256)), 0, []
 
     for i in range(256):
         j = (j + S[i] + ord(key[i % len(key)])) % 256
